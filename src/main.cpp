26f6001e5bf0dd1cad39834b9489cc71ac0961d6
283572254 <96158209+283572254@users.noreply.github.com>
2024-06-11 21:28:48 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,40 @@
-#include <Arduino.h>
-#include <BluetoothSerial.h>
-#include "light.h"
-#include <string.h>
- 
+#include "Arduino.h"
+#include "BluetoothSerial.h"
 
-/***********************************************************/
+// 检查是否有BluetoothSerial库
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
 BluetoothSerial SerialBT;
-uint16_t    u16Light_Value = 0;
-float       fLight_precent = 0;
-uint8_t     u8SerialRead[100];
-int        read_val = 0;
-#define RXBUFFERSIZE  256     //最大接收字节数
-char RxBuffer[RXBUFFERSIZE];   //接收数据
-uint8_t aRxBuffer;			//接收中断缓冲
-uint8_t Uart1_Rx_Cnt = 0;		//接收缓冲计数
-/***********************************************************/
 
-void setup()
-{
-    Serial.begin(115200);
-    // 初始化LEDC
-    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
-    ledcAttachPin(LEDC_PIN, LEDC_CHANNEL_0);
-    SerialBT.begin("BAKUMAN"); // 如果没有参数传入则默认是蓝牙名称是: "ESP32"
-    // SerialBT.setPin("1234");   // 蓝牙连接的配对码
-    Serial.printf("BT initial ok and ready to pair. \r\n");
-
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_BT_Device"); // 设置蓝牙设备名称
+  Serial.println("Bluetooth device is ready to pair");
 }
 
-void Process_Recevie()
-{
-    aRxBuffer = SerialBT.read();
-    read_val = aRxBuffer - '0';
-    if(Uart1_Rx_Cnt >= 255)  //溢出判断
-	{
-		Uart1_Rx_Cnt = 0;
-		memset(RxBuffer,0x00,sizeof(RxBuffer));
-	}
-    else
-    {
-        RxBuffer[Uart1_Rx_Cnt++] = read_val;   //接收数据转存
-        if ((RxBuffer[0]==0XA5)&&(RxBuffer[Uart1_Rx_Cnt-1]==0X5A))
-        {
-            for (int i = 0; i < 7; i++)
-            {
-                /* code */
-                Serial.write(RxBuffer[i]);
-            }
-            
-            
-            memcpy(u8SerialRead,RxBuffer,sizeof(char)*Uart1_Rx_Cnt);
-            Uart1_Rx_Cnt = 0;
-        }
-        //Serial.write(aRxBuffer);
+void loop() {
+  if (SerialBT.available()) {
+    // 创建一个字符串来保存接收到的16进制数据
+    String hexData = "";
+
+    // 当有数据可读时，继续读取
+    while (SerialBT.available()) {
+      int byteRead = SerialBT.read(); // 读取一个字节
+
+      // 将读取到的字节转换为16进制字符串
+      char hexStr[3]; // 两个字符加上null终止符
+      sprintf(hexStr, "%02X", byteRead); // 格式化为两位16进制数
+
+      // 将转换后的16进制字符串添加到hexData字符串中
+      hexData += hexStr;
+      hexData += " "; // 为了可读性，在每个字节之间添加一个空格
     }
 
-
-}
-
-
-void loop()
-{
-    if (Serial.available())
-    {
-        
-    
-        
-
-    }
-    if (SerialBT.available())
-    {   
-        //read_val = SerialBT.read();
-        Process_Recevie();
-        //Serial.write(read_val);
-    }
-
-
-    ledcWrite(LEDC_CHANNEL_0, u16Light_Value);
-    
-    delay(1);
-
-
-
+    // 打印接收到的16进制数据
+    Serial.print("Received data in hex: ");
+    Serial.println(hexData);
+  }
+  delay(100); // 稍微延迟一下，以免过于频繁地读取
 }
