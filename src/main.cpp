adc229656416f81d1c78cbc946326da58ad88f4e
283572254 <96158209+283572254@users.noreply.github.com>
2024-06-11 22:12:56 +0800

    uint8装uint16

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,46 @@
 #include "Arduino.h"
 #include "BluetoothSerial.h"
 
-// 检查是否有BluetoothSerial库
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;
 
+// 定义一个short型数组来存储接收到的数据
+// 数组大小为512，因为每个short可以存储两个字节，所以可以存储1024个字节
+short receivedData[512];
+int dataIndex = 0; // 用于跟踪数组中当前存储位置的索引
+
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32_BT_Device"); // 设置蓝牙设备名称
-  Serial.println("Bluetooth device is ready to pair");
+  SerialBT.begin("ESP32test"); // 蓝牙设备名称
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-  if (SerialBT.available()) {
-    // 创建一个字符串来保存接收到的16进制数据
-    String hexData = "";
+  static byte byteBuffer[2]; // 用于暂存两个字节
+  static int bufferIndex = 0; // byteBuffer的索引
 
-    // 当有数据可读时，继续读取
-    while (SerialBT.available()) {
-      int byteRead = SerialBT.read(); // 读取一个字节
+  // 检查是否有从蓝牙接收到的数据
+  while (SerialBT.available() > 0 && dataIndex < 512) {
+    int byteRead = SerialBT.read(); // 读取一个字节
 
-      // 将读取到的字节转换为16进制字符串
-      char hexStr[3]; // 两个字符加上null终止符
-      sprintf(hexStr, "%02X", byteRead); // 格式化为两位16进制数
+    // 检查read()是否真的读取到了数据
+    if (byteRead != -1) {
+      byteBuffer[bufferIndex++] = (byte)byteRead; // 存储到buffer中
 
-      // 将转换后的16进制字符串添加到hexData字符串中
-      hexData += hexStr;
-      hexData += " "; // 为了可读性，在每个字节之间添加一个空格
+      // 检查是否收集了两个字节
+      if (bufferIndex == 2) {
+        // 将两个字节合并为一个short型变量
+        // 注意：这里可能需要根据实际的字节序来调整
+        receivedData[dataIndex++] = (short)((byteBuffer[0] << 8) | byteBuffer[1]);
+
+        // 重置bufferIndex以便于接收下一对字节
+        bufferIndex = 0;
+      }
     }
-
-    // 打印接收到的16进制数据
-    Serial.print("Received data in hex: ");
-    Serial.println(hexData);
   }
-  delay(100); // 稍微延迟一下，以免过于频繁地读取
+  
+  // 在这里可以处理接收到的short型数组数据
 }
